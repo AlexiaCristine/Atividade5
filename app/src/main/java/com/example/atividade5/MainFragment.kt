3c14d788594fc9ab055e8ea685fa27a8b3e153ea
Desktop <a.alexiac22@gmail.com>
2021-04-16 10:53:06 -0300

    Sem testar, com tradução e navegação feitas

--- app/src/main/java/com/example/atividade5/MainFragment.kt
+++ app/src/main/java/com/example/atividade5/MainFragment.kt
@@ -17,16 +17,6 @@ class MainFragment : Fragment(), View.OnClickListener {
 
 
     var navController: NavController? = null
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,17 +26,6 @@ class MainFragment : Fragment(), View.OnClickListener {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    companion object {
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
