b38cbf346a3ee187393e0d45df8905f9315910d1
Desktop <a.alexiac22@gmail.com>
2021-04-19 23:14:15 -0300

    com tradução e navegação feitas, testado e com login e registro

--- app/src/main/java/com/example/atividade5/MainActivity.kt
+++ app/src/main/java/com/example/atividade5/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.atividade5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Build
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.annotation.ColorInt
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener{
+
+            if (edUsername.text.trim().isNotEmpty() || edPassword.text.trim().isNotEmpty()){
+                Toast.makeText(this, "Input provided", Toast.LENGTH_LONG).show()
+
+            }
+            else{
+
+            Toast.makeText(this, "input required", Toast.LENGTH_LONG).show();
+
+        }
+
+        }
+            tvRegister.setOnClickListener{
+                val intent = Intent( this, RegisterActivity::class.java)
+                startActivity(intent)
+
+            }
+
     }
 }
+
---
+++ app/src/main/java/com/example/atividade5/RegisterActivity.kt
@@ -0,0 +1,39 @@
+package com.example.atividade5
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        btnRegister.setOnClickListener{
+
+            if(editEmail.text.trim().isNotEmpty() || editPassword.text.trim().isNotEmpty() || editCPassword.text.trim().isNotEmpty() ){
+
+                Toast.makeText(this, "Input provided", Toast.LENGTH_LONG).show()
+
+
+            }else{
+
+                Toast.makeText(this, "Input required", Toast.LENGTH_LONG).show()
+                
+            }
+
+        }
+
+
+
+        tvLogin.setOnClickListener{
+
+            val intent = Intent( this, MainActivity::class.java )
+            startActivity(intent)
+
+        }
+
+    }
+}
